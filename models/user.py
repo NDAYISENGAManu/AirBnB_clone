1ec833da6abfb9dc4b1d630420cb1d9be07a5671
BySamm <sam.mba.byiringiro@gmail.com>
2023-11-12 23:25:41 +0200

    Back to normal

--- models/user.py
+++ models/user.py
@@ -16,4 +16,4 @@ class User(BaseModel):
     email = ""
     password = ""
     first_name = ""
-    last_name = ""
+    last_name = "" 
--- tests/test_console.py
+++ tests/test_console.py
@@ -12,9 +12,11 @@ Unittest classes:
     And
     TestHBNBCommand_update
 """
-
+#export PYTHONPATH = "models"
 import os
 import sys
+path = os.path.abspath("models")
+sys.path.append(path)
 import unittest
 from models import storage
 from models.engine.file_storage import FileStorage
